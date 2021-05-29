96044f11d25df49ebb6029a67b173bc4d1e37392
Samarth Gupta <53513648+samarthgupta19@users.noreply.github.com>
2021-05-29 16:01:58 +0530

    Added All Codes

---
+++ Cycle Sheet - 3/Q2.js
@@ -0,0 +1,9 @@
+// 19BDS0042 SAMARTH GUPTA
+
+
+var http = require('http');  var fs = require('fs');
+http.createServer(function (req, res)
+{
+fs.readFile('Q2.html', function(err, data) {  res.writeHead(200, {'Content-Type': 'text/html'});  res.write(data);
+res.end();
+});}).listen(8080);
