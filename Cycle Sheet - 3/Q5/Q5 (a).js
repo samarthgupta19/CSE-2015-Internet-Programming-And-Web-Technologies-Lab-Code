79469c563353102ba1a3aff49d84060a471a6f37
Samarth Gupta <53513648+samarthgupta19@users.noreply.github.com>
2021-06-01 16:30:49 +0530

    Added All Codes

--- Cycle Sheet - 3/Q5/Q5 (a).js
+++ Cycle Sheet - 3/Q5/Q5 (a).js
@@ -5,7 +5,6 @@ if (err) throw err;
 var dbo = db.db("q5");
 var myobj = [
 { name: 'Samarth', age: 19 , dob: "2002-04-19", yearofad: 2019 },
-19BDS0042 SAMARTH GUPTA
 { name: 'Kevin', age: 20 , dob: "2001-05-15", yearofad: 2018 },
 { name: 'Harsh', age: 26 , dob: "1995-06-01", yearofad: 2012 },
 { name: 'Arnav', age: 21 , dob: "2000-03-22", yearofad: 2017 },
